303540bd23b945cdebcca6d0bc3720f3769a68ed
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2016-07-31 22:24:20 -0700

    Render a red pixel on the screen

--- c/main.cpp
+++ c/main.cpp
@@ -1,13 +1,73 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+void log_sdl_error(const std::string &msg) {
+  std::cout << msg << " error: " << SDL_GetError() << std::endl;
+}
+
 
 int main (int, char**) {
+  SDL_Window *win;
+  SDL_Renderer *ren;
+  SDL_Event event;
+  bool quit = false;
+
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    log_sdl_error("SDL_Init");
     return 1;
   }
 
+  win = SDL_CreateWindow("Mandelbrot set", 100, 100,
+                         SCREEN_WIDTH, SCREEN_HEIGHT,
+                         SDL_WINDOW_SHOWN);
+  if (win == nullptr) {
+    log_sdl_error("SDL_CreateWindow");
+    SDL_Quit();
+    return 1;
+  }
+
+  ren = SDL_CreateRenderer(win, -1, (SDL_RENDERER_ACCELERATED |
+                                     SDL_RENDERER_PRESENTVSYNC));
+
+  if (ren == nullptr) {
+    SDL_DestroyWindow(win);
+    log_sdl_error("SDL_CreateRenderer");
+    SDL_Quit();
+    return 1;
+  }
+
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+
+      if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+        case SDLK_ESCAPE:
+          quit = true;
+          break;
+        default:
+          break;
+        }
+      }
+
+      if (event.type == SDL_MOUSEBUTTONDOWN) {
+        quit = true;
+      }
+    }
+
+    SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+    SDL_RenderDrawPoint(ren, 400, 300);
+    SDL_RenderPresent(ren);
+  }
+
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
   SDL_Quit();
   return 0;
 }
